af22894908b14bc0458fcb67faf248a2f1d05892
Tokeiya <net_seed@hotmail.com>
2025-11-28 18:39:58 +0900

    Add `TypeDescriptor` struct and methods for type-specific field handling in `dsv_writer`
    - Introduced `TypeDescriptor` struct with support for field management and description.
    - Implemented methods: `push`, `delete`, `describe`, `fill`, and `append`.

--- dsv_writer/src/type_descriptor.rs
+++ dsv_writer/src/type_descriptor.rs
@@ -1,1 +1,32 @@
+use crate::field_description::Description;
+use crate::field_descriptor::FieldDescriptor;
 
+pub struct TypeDescriptor<'a, T>(Vec<FieldDescriptor<'a, T>>);
+
+impl<'a, T> Default for TypeDescriptor<'a, T> {
+	fn default() -> Self {
+		todo!()
+	}
+}
+
+impl<'a, T> TypeDescriptor<'a, T> {
+	pub fn push<F: Fn(&T) -> Description<'_> + 'a>(&mut self, f: F) -> usize {
+		todo!()
+	}
+
+	pub fn delete(idx: usize) -> Description<'a> {
+		todo!()
+	}
+
+	pub fn describe(&self, value: &T) -> Vec<Description> {
+		todo!()
+	}
+
+	pub fn fill(&self, value: &T, buff: &mut Vec<Description>) -> usize {
+		todo!()
+	}
+
+	pub fn append(&self, value: &T, buff: &mut Vec<Description>) -> usize {
+		todo!()
+	}
+}
