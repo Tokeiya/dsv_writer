5ef45bd4ed70c21864f822e1b8cb8ca86655ff7c
Tokeiya <net_seed@hotmail.com>
2025-11-29 19:31:35 +0900

    Clean up `playground` main function by removing unused imports and example logic

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,31 +1,3 @@
 mod poc_selector;
 
-use dsv_writer::field_description::Description;
-use dsv_writer::primitive_writer::QuoteMode;
-use poc_selector::*;
-
-pub struct Point {
-	x: i32,
-	y: i32,
-}
-
-fn main() {
-	let mut vec: Vec<Selector<Point>> = Vec::new();
-
-	let x =
-		Selector::new(|p: &Point| Description::new_string(p.x.to_string(), QuoteMode::AutoDetect));
-
-	let y =
-		Selector::new(|p: &Point| Description::new_string(p.y.to_string(), QuoteMode::AutoDetect));
-
-	vec.push(x);
-	vec.push(y);
-
-	let point = Point { x: 1, y: 2 };
-
-	for s in vec.iter() {
-		println!("{:?}", s.select(&point));
-	}
-
-	println!("Hello, world!");
-}
+fn main() {}
