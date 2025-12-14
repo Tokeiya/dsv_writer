e95dc40383d5431f16170f1a4b615a2c63afe82d
tokeiya3@Home <net_seed@hotmai.com>
2025-12-14 19:58:02 +0900

    Refactor: Introduce `AsyncFoo` and `DynAsyncFoo` traits with `impl Future` and `Pin` support, add blanket implementation for compatibility.

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,15 +1,15 @@
-enum Foo {
-	Hoge,
-	Piyo,
+use std::pin::Pin;
+
+pub trait AsyncFoo {
+	fn foo(&self) -> impl Future<Output = i32> + Send;
+}
+pub trait DynAsyncFoo {
+	fn foo(&self) -> Pin<Box<dyn Future<Output = i32> + Send + '_>>;
 }
 
-fn main() {
-	let s = r##""""##.to_string();
-	println!("{}", &s);
-
-	println!("{}", s.as_str().replace("\"", r#""""#))
+impl<T: AsyncFoo + Send> DynAsyncFoo for T {
+	fn foo(&self) -> Pin<Box<dyn Future<Output = i32> + Send + '_>> {
+		Box::pin(self.foo())
+	}
 }
-
-fn f(a: &Foo) {
-	if let Foo::Hoge = a {}
-}
+fn main() {}
