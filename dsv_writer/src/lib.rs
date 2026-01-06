d141f4e936bcf73a2e20d1be793a545165f156cc
tokeiya3@Home <net_seed@hotmai.com>
2026-01-06 17:33:53 +0900

    chore: update `lib.rs` exports to expose additional modules and types for improved usability

--- dsv_writer/src/lib.rs
+++ dsv_writer/src/lib.rs
@@ -7,5 +7,10 @@ mod raw_encoder_error;
 mod raw_writer;
 mod to_dsv;
 
+pub use crate::quote_mode::QuoteMode;
+pub use crate::raw_async_encoder::RawAsyncEncode;
+pub use crate::raw_async_writer::RawAsyncWriter;
+pub use crate::raw_encoder::Encoder;
+pub use crate::raw_encoder_error::{Error, Result};
 pub use crate::raw_writer::RawWriter;
 pub use crate::to_dsv::ToDsv;
