2dc7b95f35f68f92e9608e0bd56f390395a1fb22
tokeiya3@Home <net_seed@hotmai.com>
2026-03-25 21:08:11 +0900

    Revert "Merge remote-tracking branch 'origin/master' into raw_encoder_refacting"
    This reverts commit 349974af0f8676dde24c797cb3233b3d50dd1edc, reversing
    changes made to d141f4e936bcf73a2e20d1be793a545165f156cc.

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,25 +1,15 @@
-#[cfg(target_arch = "x86_64")]
-pub mod cmp_mask;
-#[cfg(target_arch = "x86_64")]
-pub mod cmpestri;
+use std::pin::Pin;
 
-pub mod sample_generator;
-pub mod scalar;
-pub mod shared;
-pub mod should_quote_datum;
-
-#[cfg(target_arch = "aarch64")]
-pub mod neon;
-
-const TXT: &str = "Have a nice day!";
-fn main() {
-	let vec = sample_generator::gen_sample(114514, 2_000_000, 30.0, 5.0, 0.1, 0.1);
-
-	for elem in vec.iter().take(10) {
-		println!("-------------------------");
-		println!("{}", elem);
-		println!("-------------------------");
-	}
-
-	println!("{}", vec.iter().map(|s| s.len()).sum::<usize>())
+pub trait AsyncFoo {
+	fn foo(&self) -> impl Future<Output = i32> + Send;
 }
+pub trait DynAsyncFoo {
+	fn foo(&self) -> Pin<Box<dyn Future<Output = i32> + Send + '_>>;
+}
+
+impl<T: AsyncFoo + Send> DynAsyncFoo for T {
+	fn foo(&self) -> Pin<Box<dyn Future<Output = i32> + Send + '_>> {
+		Box::pin(self.foo())
+	}
+}
+fn main() {}
